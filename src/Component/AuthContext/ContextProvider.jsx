8ee82b4d880a540a1f62ab7acd6036c5c43400cd
najima1 <mohammednazim3629@gmail.com>
2022-11-08 09:12:41 -0600

    login with google done

--- src/Component/AuthContext/ContextProvider.jsx
+++ src/Component/AuthContext/ContextProvider.jsx
@@ -2,13 +2,16 @@ import React, { createContext } from "react";
 import {
   createUserWithEmailAndPassword,
   getAuth,
+  GoogleAuthProvider,
   signInWithEmailAndPassword,
+  signInWithPopup,
   updateProfile,
 } from "firebase/auth";
 import app from "../Firebase/Firebase";
 
 export const AuthContext = createContext();
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 
 const ContextProvider = ({ children }) => {
   //create user with email & password
@@ -21,13 +24,23 @@ const ContextProvider = ({ children }) => {
     return signInWithEmailAndPassword(auth, email, password);
   };
 
+  //login with google
+  const login_with_google = () => {
+    return signInWithPopup(auth, googleProvider);
+  };
+
   //updata user profile & name
   const updata_user = (profile) => {
     return updateProfile(auth.currentUser, profile);
   };
 
   //provide all firebase method
-  const userInfo = { signUpUser, updata_user, user_login_firebase };
+  const userInfo = {
+    signUpUser,
+    updata_user,
+    user_login_firebase,
+    login_with_google,
+  };
 
   return (
     <div>
